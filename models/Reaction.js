e857ec2e11d38b1346f6c442aaebd1b84ec81a9c
Michaela Varberg <michaelavarberg@gmail.com>
2022-09-28 09:33:43 -0600

    created Reaction schema

---
+++ models/Reaction.js
@@ -0,0 +1,22 @@
+const { Schema } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: new Schema.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxLength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //add getter method to format timestamp on query
+  },
+});
