dd5f41f0b18188d8c707efd1465be75d2f8204ee
Michaela Varberg <michaelavarberg@gmail.com>
2022-09-28 09:30:44 -0600

    created thought schema

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require("mongoose");
+const Reaction = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      //add getter method to format timestamp on query
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [Reaction],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,26 +1,33 @@
 const { Schema, model } = require("mongoose");
 
 //Schema to create User model
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: [validateEmail, "Please provide a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: [validateEmail, "Please provide a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please provide a valid email address",
-    ],
-  },
-  thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
-  friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
-});
+  {
+    toJSON: {
+      virtuals: true,
+    },
+  }
+);
 
 userSchema.virtual("friendCount").get(function () {
   return `${this.friends.length}`;
